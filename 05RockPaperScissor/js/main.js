39ed1c2fe7c9cc19e374a00e44e7371ff46c23b8
shrawankansi <shrawankansi@gmail.com>
2023-01-19 23:52:14 +0530

    update

--- 05RockPaperScissor/js/main.js
+++ 05RockPaperScissor/js/main.js
@@ -1,11 +1,11 @@
 let imgclick = document.querySelector('.choice').innerHTML;
 let r, p, s;
 if(r>p){
-    console.log("rok win");
-}
-else if(s>p<r){
-    console.log("seaser win");
-}
-else{
-    console.log("paper win");
+//     console.log("rok win");
+// }
+// else if(s>p<r){
+//     console.log("seaser win");
+// }
+// else{
+//     console.log("paper win");
 }
