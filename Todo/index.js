b9cf1bb3bb1439e0f3c61fdee3f2d1bb61b06287
shrawankansi <shrawankansi@gmail.com>
2023-01-31 23:10:55 +0530

    update todo app with awesome ui

--- Todo/index.js
+++ Todo/index.js
@@ -17,7 +17,7 @@ document.querySelector(".addButton") .onclick = function () {
                     </button>
             </div>`;
 
-            var current_tasks = document.querySelector('.delete');
+            var current_tasks = document.querySelectorAll('.delete');
             for(var i=0; i<current_tasks.length; i++){
                 current_tasks[i].onclick = function () {
                     this.parentNode.removeChild();
